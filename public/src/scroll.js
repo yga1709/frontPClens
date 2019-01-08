aa624544ddaae714000995d5bee6754ad503fc57
a1709 <ygbox0324@gmail.com>
2019-01-08 12:48:07 +0900

    add : Realtime comment get.

--- public/src/scroll.js
+++ public/src/scroll.js
@@ -1,23 +1,47 @@
-let nico = new nicoJS({
-  app: document.getElementById("app"),
-  width: 600,
-  height: 400,
-  font_size: 50, // opt
-  color: "#fff" // opt
-});
-//nico.loop(["comment_1", "comment_2", "comment_3", "comment_4"]);
-nico.listen();
-
-db.collection("pclens")
-  .where("url", "==", url)
-  .onSnapshot(function(snapshot) {
-    snapshot.docChanges.forEach(function(change) {
-      if (change.type === "added") {
-        nico.send(change.comment);
-        console.log(change.comment);
-      }
-      if (change.type === "removed") {
-        console.log(`User ${change.doc.id} has gone offline.`);
-      }
-    });
+document.getElementById("get").onclick = () => {
+  let nico = new nicoJS({
+    app: document.getElementById("app"),
+    width: 600,
+    height: 400,
+    font_size: 50, // opt
+    color: "#fff" // opt
   });
+  nico.listen();
+  const url = document.getElementById("url").value;
+  db.collection("pclens")
+    .where("url", "==", url)
+    .get()
+    .then(querySnapshot => {
+      querySnapshot.forEach(doc => {
+        console.log(`${doc.id} => ${doc.data().comment}`);
+        nico.send(doc.data().comment);
+      });
+    });
+
+  /*db.collection("pclens")
+    .where("url", "==", url)
+    .onSnapshot(function(querySnapshot) {
+      querySnapshot.forEach(doc => {
+        console.log(`${doc.id} => ${doc.data().comment}`);
+        if (doc.type === "added") {
+          console.log(doc.type);
+        }
+      });
+    });*/
+  db.collection("pclens")
+    .where("url", "==", url)
+    .onSnapshot(snapshot => {
+      snapshot.docChanges().forEach(change => {
+        if (change.type === "added") {
+          console.log("New city: ", change.doc.data());
+          nico.send(change.doc.data().comment);
+        }
+        if (change.type === "modified") {
+          console.log("Modified city: ", change.doc.data());
+        }
+        if (change.type === "removed") {
+          console.log("Removed city: ", change.doc.data());
+        }
+      });
+    });
+};
