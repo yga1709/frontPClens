e6d3689d05460d7625a07ccaad7f55579b251d1a
a1709 <ygbox0324@gmail.com>
2019-01-17 16:48:47 +0900

    delete : nicoJS

--- public/src/getDB.js
+++ public/src/getDB.js
@@ -1,12 +1,4 @@
 document.getElementById("get").onclick = () => {
-  const nico = new nicoJS({
-    app: document.getElementById("app"),
-    width: 600,
-    height: 400,
-    font_size: 50, // opt
-    color: "#fff" // opt
-  });
-  nico.listen();
   const url = document.getElementById("url").value;
   db.collection("pclens")
     .where("url", "==", url)
